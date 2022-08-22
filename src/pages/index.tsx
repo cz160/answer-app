f82d17db2b028b0f8f71b5a4642f098013f505d8
cz160 <1604506998@qq.com>
2022-08-22 15:48:43 +0800

    feat: 首页完成

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,8 +4,10 @@ import { Button } from 'antd-mobile';
 export default function Home() {
   return (
     <div className={ss.home}>
-      <h1 className={ss.title}>欢迎来到学习答题系统</h1>
-      <Button type="primary">111</Button>
+      <h1 className={ss.title}>性格分析系统</h1>
+      <Button className={ss.btn} type="primary">
+        开始测试
+      </Button>
     </div>
   );
 }
